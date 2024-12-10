2aa67a7334f644cd45e173a93bab81c928c09654
mishelle-quinchiguango <55854734+mishelle-quinchiguango@users.noreply.github.com>
2024-12-10 13:06:02 +0100

    Update sqs.py

--- sqs.py
+++ sqs.py
@@ -1,10 +1,25 @@
 import boto3
 
+# Crear un cliente de SQS
 sqs = boto3.client('sqs')
 
+QUEUE_NAME = 'imagen-cargada-queue'
 
-def create_sqs_queue():
-    response = sqs.create_queue(QueueName='thumbnail-queue')
-    print(f"Cola creada: {response['QueueUrl']}")
+def crear_cola_sqs():
+    try:
+        # Crear la cola de SQS
+        response = sqs.create_queue(
+            QueueName=QUEUE_NAME,
+            Attributes={
+                'DelaySeconds': '0',
+                'MessageRetentionPeriod': '345600',  
+            }
+        )
+        print(f"Cola '{QUEUE_NAME}' creada exitosamente.")
+        print(f"URL de la cola: {response['QueueUrl']}")
+        return response['QueueUrl']
+    except Exception as e:
+        print(f"Error al crear la cola de SQS: {e}")
 
-create_sqs_queue()
+if __name__ == "__main__":
+    crear_cola_sqs()
